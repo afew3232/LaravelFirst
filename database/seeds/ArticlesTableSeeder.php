49b5ccb9c8d5e7ca5f13e98f3b6278c003aeb03a
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-08-03 18:38:27 +0900

    [Add] seed

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("articles")->delete();//Query Builder を使って、Articlesテーブルのレコードを全て削除。
+
+        $faker = Faker\Factory::create('en_US');// Faker を使用してダミーデータを作成しています。Laravel では標準で Faker が使用できるようになっています。
+
+        for ($i = 0; $i < 10; $i++) {    // ③
+            App\Article::create([
+                'title' => $faker->sentence(),
+                'body' => $faker->paragraph()
+            ]);
+        }
+
+    }
+}
---
+++ storage/framework/views/22f33417a7051618ddefbcd4a7e455823a6ee9eb.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>My Blog</title>
+</head>
+<body>
+	<?php echo $__env->yieldContent("content"); ?>
+</body>
+</html><?php /**PATH C:\Users\Shouichi\Desktop\work\laravel\test\resources\views/layout.blade.php ENDPATH**/ ?>
